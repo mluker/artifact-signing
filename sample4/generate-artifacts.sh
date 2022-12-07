d250a393504812e119761b67eef9044250aa03e8
Matt Luker <luker.matt@gmail.com>
2022-12-07 01:08:53 +0000

    fixed notation / oras version updates changing file formats

--- sample4/generate-artifacts.sh
+++ sample4/generate-artifacts.sh
@@ -21,7 +21,7 @@ sbom-tool generate -b ./assets -bc ./src/myapp -pn ${REPONAME} -m ./sboms -pv 1.
 oras attach --artifact-type $SBOM_ARTIFACT_TYPE $IMAGE ./sboms/_manifest/spdx_2.2/manifest.spdx.json
 
 # sign sbom
-notation sign $IMAGE@$(oras discover -o json --artifact-type $SBOM_ARTIFACT_TYPE $IMAGE | jq -r ".referrers[0].digest") -k $REGISTRY
+notation sign $IMAGE@$(oras discover -o json --artifact-type $SBOM_ARTIFACT_TYPE $IMAGE | jq -r ".manifests[0].digest") -k $REGISTRY
 
 # discover the image to show the tree in the console
 oras discover $IMAGE -o tree
